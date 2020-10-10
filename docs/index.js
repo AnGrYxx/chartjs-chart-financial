f78b08719d52162e3a193b9bdcf87e2a01ba14a9
Tarmo <tarmo888@hotmail.com>
2020-10-11 01:04:40 +0300

    not enough data

--- docs/index.js
+++ docs/index.js
@@ -78,9 +78,9 @@ var update = function() {
 
 	if(!!fromDate || !!toDate || !!period || !!token){
 		getDataGrd(period, fromDate, toDate, token, function(json) {
-			if (!Object.keys(json).length) {
+			if (Object.keys(json).length < 2) {
 				json = [{}];
-				alert('data not available');
+				alert('not enough data available');
 			}
 			dataset.label = `${token.toUpperCase()} Price [GBYTE]`;
 			dataset.data = json;
