1c58c1f89e0fe7bdda75de39f600b060947c863a
MYACTIVITIES\Antonius <antonius.gress@myactivities.com>
2020-10-10 08:34:47 +0200

    UI improvements + different tokens added

--- docs/index.js
+++ docs/index.js
@@ -1,6 +1,6 @@
-function getDataGrd(period, start, end) {
+function getDataGrd(period, start, end, token) {
 	
-	return fetch(`https://data.ostable.org/api/v1/candles/GRD-GBYTE/?period=${period}&start=${start}&end=${end}`)
+	return fetch(`https://data.ostable.org/api/v1/candles/${token.toUpperCase()}-GBYTE/?period=${period}&start=${start}&end=${end}`)
 		.then(response => {
 			return response.json()
 		})
@@ -25,7 +25,7 @@ function getDataGrd(period, start, end) {
 				type: 'candlestick',
 				data: {
 					datasets: [{
-						label: 'GRD Price [GBYTE]',
+						label: `${token.toUpperCase()} Price [GBYTE]`,
 						data: json
 					}]
 				}
@@ -42,16 +42,21 @@ function getDataGrd(period, start, end) {
 				// period
 				var period = document.getElementById('period').value;
 
-				if(!!fromDate || !!toDate || !!period){
+				// token
+				var token = document.getElementById('token').value;
+
+
+				if(!!fromDate || !!toDate || !!period || !!token){
 					console.log('###');
 					console.log(period);
 					console.log(fromDate);
 					console.log(toDate);
+					console.log(token);
 					if(!toDate) {
 						console.log('!toDate');
-						getDataGrd(period, fromDate, moment().format('YYYY-MM-DD'));
+						getDataGrd(period, fromDate, moment().format('YYYY-MM-DD'), token);
 					} else {
-						getDataGrd(period, fromDate, toDate);
+						getDataGrd(period, fromDate, toDate, token);
 					} 
 					
 				}
@@ -96,80 +101,5 @@ function getDataGrd(period, start, end) {
 		})
 }
 
-getDataGrd('daily', '2020-09-22', moment().format('YYYY-MM-DD'));
+getDataGrd('daily', '2020-09-22', moment().format('YYYY-MM-DD'), 'GRD');
 
-
-// function getDataGrb() {
-// 	return fetch('https://data.ostable.org/api/v1/candles/GRB-GBYTE/?period=daily&start=2020-09-22&end=2020-10-09')
-// 		.then(response => {
-// 			return response.json()
-// 		})
-// 		.then(json => {
-// 			console.log(json);
-// 			return json.map(item => {
-// 				return {
-// 					t: parseFloat(moment(item.start_timestamp).format('x')),
-// 					o: item.open_price,
-// 					h: item.highest_price,
-// 					l: item.lowest_price,
-// 					c: item.close_price
-// 				}
-// 			})
-// 		})
-// 		.then(json => {
-// 			console.log(json);
-// 			var ctx = document.getElementById('chartgrb').getContext('2d');
-// 			ctx.canvas.width = 1000;
-// 			ctx.canvas.height = 250;
-// 			var chart = new Chart(ctx, {
-// 				type: 'candlestick',
-// 				data: {
-// 					datasets: [{
-// 						label: 'GRB Price [GBYTE]',
-// 						data: json
-// 					}]
-// 				}
-// 			});
-// 			return json;
-// 		})
-// }
-
-// getDataGrb();
-
-
-// function getDataGrau() {
-// 	return fetch('https://data.ostable.org/api/v1/candles/GRAU-GBYTE/?period=daily&start=2020-09-22&end=2020-10-09')
-// 		.then(response => {
-// 			return response.json()
-// 		})
-// 		.then(json => {
-// 			console.log(json);
-// 			return json.map(item => {
-// 				return {
-// 					t: parseFloat(moment(item.start_timestamp).format('x')),
-// 					o: item.open_price,
-// 					h: item.highest_price,
-// 					l: item.lowest_price,
-// 					c: item.close_price
-// 				}
-// 			})
-// 		})
-// 		.then(json => {
-// 			console.log(json);
-// 			var ctx = document.getElementById('chartgrau').getContext('2d');
-// 			ctx.canvas.width = 1000;
-// 			ctx.canvas.height = 250;
-// 			var chart = new Chart(ctx, {
-// 				type: 'candlestick',
-// 				data: {
-// 					datasets: [{
-// 						label: 'GRAU Price [GBYTE]',
-// 						data: json
-// 					}]
-// 				}
-// 			});
-// 			return json;
-// 		})
-// }
-
-// getDataGrau();
