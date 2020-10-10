33cc705d34a89321f4cfdf021e2d6ac201a7e98e
Tarmo <tarmo888@hotmail.com>
2020-10-11 00:15:57 +0300

    fix chart reuse

--- docs/index.js
+++ docs/index.js
@@ -1,6 +1,14 @@
 var ctx = document.getElementById('chart').getContext('2d');
 ctx.canvas.width = window.innerWidth/2;
 ctx.canvas.height = window.innerHeight/2;
+var chart = new Chart(ctx, {
+	type: 'candlestick',
+	data: {
+		datasets: [{
+			data: [{}]
+		}]
+	}
+});
 
 function getDataGrd(period, start, end, token, callback) {
 	
@@ -23,6 +31,42 @@ function getDataGrd(period, start, end, token, callback) {
 }
 
 var update = function () {
+	var dataset = chart.config.data.datasets[0];
+	if (dataset.data.length == 1) dataset.data.pop();
+
+	// candlestick vs ohlc
+	var type = document.getElementById('type').value;
+	dataset.type = type;
+
+	// linear vs log
+	var scaleType = document.getElementById('scale-type').value;
+	chart.config.options.scales.y.type = scaleType;
+
+	// color
+	var colorScheme = document.getElementById('color-scheme').value;
+	if (colorScheme === 'neon') {
+		dataset.color = {
+			up: '#01ff01',
+			down: '#fe0000',
+			unchanged: '#999',
+		};
+	} else {
+		delete dataset.color;
+	}
+
+	// border
+	var border = document.getElementById('border').value;
+	var defaultOpts = Chart.defaults.elements[type];
+	if (border === 'true') {
+		dataset.borderColor = defaultOpts.borderColor;
+	} else {
+		dataset.borderColor = {
+			up: defaultOpts.color.up,
+			down: defaultOpts.color.down,
+			unchanged: defaultOpts.color.up
+		};
+	}
+
 	// from date
 	var fromDate = document.getElementById('fromDate').value;
 	// to date
@@ -34,49 +78,8 @@ var update = function () {
 
 	if(!!fromDate || !!toDate || !!period || !!token){
 		getDataGrd(period, fromDate, toDate, token, function(json) {
-			dataset = {
-				label: `${token.toUpperCase()} Price [GBYTE]`,
-				data: json
-			};
-
-			// candlestick vs ohlc
-			var type = document.getElementById('type').value;
-
-
-			// color
-			var colorScheme = document.getElementById('color-scheme').value;
-			if (colorScheme === 'neon') {
-				dataset.color = {
-					up: '#01ff01',
-					down: '#fe0000',
-					unchanged: '#999',
-				};
-			}
-
-			// border
-			var border = document.getElementById('border').value;
-			var defaultOpts = Chart.defaults.elements[type];
-			if (border === 'true') {
-				dataset.borderColor = defaultOpts.borderColor;
-			} else {
-				dataset.borderColor = {
-					up: defaultOpts.color.up,
-					down: defaultOpts.color.down,
-					unchanged: defaultOpts.color.up
-				};
-			}
-
-			var chart = new Chart(ctx, {
-				type: type,
-				data: {
-					datasets: [dataset]
-				}
-			});
-
-			// linear vs log
-			var scaleType = document.getElementById('scale-type').value;
-			chart.config.options.scales.y.type = scaleType;
-
+			dataset.label = `${token.toUpperCase()} Price [GBYTE]`;
+			dataset.data = json;
 			chart.update();
 		});
 	}
