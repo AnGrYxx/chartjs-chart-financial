c85269e1b2095e63a705d284ce1f0b93855c1c41
MYACTIVITIES\Antonius <antonius.gress@myactivities.com>
2020-10-25 12:36:49 +0100

    frontend styling, update button removed, jquery chart update, jquery document ready

--- docs/index.js
+++ docs/index.js
@@ -1,6 +1,6 @@
 var ctx = document.getElementById('chart').getContext('2d');
-ctx.canvas.width = window.innerWidth/2;
-ctx.canvas.height = window.innerHeight/2;
+ctx.canvas.width = window.innerWidth / 2;
+ctx.canvas.height = window.innerHeight / 2;
 var chart = new Chart(ctx, {
 	type: 'candlestick',
 	data: {
@@ -11,7 +11,7 @@ var chart = new Chart(ctx, {
 });
 
 function getDataGrd(period, start, end, token, callback) {
-	
+
 	return fetch(`https://data.ostable.org/api/v1/candles/${token.toUpperCase()}-GBYTE/?period=${period}&start=${start}&end=${end}`)
 		.then(response => {
 			return response.json()
@@ -30,7 +30,7 @@ function getDataGrd(period, start, end, token, callback) {
 		.then(callback)
 }
 
-var update = function() {
+var update = function () {
 	var dataset = chart.config.data.datasets[0];
 	if (dataset.data.length == 1) dataset.data.pop();
 
@@ -76,8 +76,8 @@ var update = function() {
 	// token
 	var token = document.getElementById('token').value;
 
-	if(!!fromDate || !!toDate || !!period || !!token){
-		getDataGrd(period, fromDate, toDate, token, function(json) {
+	if (!!fromDate || !!toDate || !!period || !!token) {
+		getDataGrd(period, fromDate, toDate, token, function (json) {
 			if (Object.keys(json).length < 2) {
 				json = [{}];
 				alert('not enough data available');
@@ -88,7 +88,8 @@ var update = function() {
 		});
 	}
 };
-var period = function() {
+
+var period = function () {
 	// period
 	var period = document.getElementById('period').value;
 	if (period === 'daily') {
@@ -101,10 +102,69 @@ var period = function() {
 	}
 };
 
-document.getElementById('period').addEventListener('change', period);
-document.getElementById('update').addEventListener('click', update);
 
-(function() {
+$('#period,#token,#fromDate,#toDate,#type,#scale-type,#color-scheme,#border').on('change', function () {
+	var dataset = chart.config.data.datasets[0];
+	if (dataset.data.length == 1) dataset.data.pop();
+
+	// candlestick vs ohlc
+	var type = document.getElementById('type').value;
+	dataset.type = type;
+
+	// linear vs log
+	var scaleType = document.getElementById('scale-type').value;
+	chart.config.options.scales.y.type = scaleType;
+
+	// color
+	var colorScheme = document.getElementById('color-scheme').value;
+	if (colorScheme === 'neon') {
+		dataset.color = {
+			up: '#01ff01',
+			down: '#fe0000',
+			unchanged: '#999',
+		};
+	} else {
+		delete dataset.color;
+	}
+
+	// border
+	var border = document.getElementById('border').value;
+	var defaultOpts = Chart.defaults.elements[type];
+	if (border === 'true') {
+		dataset.borderColor = defaultOpts.borderColor;
+	} else {
+		dataset.borderColor = {
+			up: defaultOpts.color.up,
+			down: defaultOpts.color.down,
+			unchanged: defaultOpts.color.up
+		};
+	}
+
+	// from date
+	var fromDate = document.getElementById('fromDate').value;
+	// to date
+	var toDate = document.getElementById('toDate').value;
+	// period
+	var period = document.getElementById('period').value;
+	// token
+	var token = document.getElementById('token').value;
+
+	if (!!fromDate || !!toDate || !!period || !!token) {
+		getDataGrd(period, fromDate, toDate, token, function (json) {
+			if (Object.keys(json).length < 2) {
+				json = [{}];
+				alert('not enough data available');
+			}
+			dataset.label = `${token.toUpperCase()} Price [GBYTE]`;
+			dataset.data = json;
+			chart.update();
+		});
+	}
+});
+
+$(document).ready(function() {
 	period();
 	update();
-})();
+});
+
+
